61be7bccaefd2994568027bddb2f87f9200cf8ad
sivaram <sivaram3710@gmail.com>
2024-10-03 11:41:03 +0530

    added dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +13,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
-			<body className={`${inter.className} antialiased`}>{children}</body>
+		<html lang="en" suppressHydrationWarning>
+			<body className={inter.className}>
+				<Providers>
+					<Navbar />
+					<main className="container py-10">{children}</main>
+				</Providers>
+			</body>
 		</html>
 	);
 }
