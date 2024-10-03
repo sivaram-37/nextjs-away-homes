600c03d37d246310576dd359d9a1087a65c0442c
sivaram <sivaram3710@gmail.com>
2024-10-03 11:21:43 +0530

    created new routes

---
+++ app/bookings/page.tsx
@@ -0,0 +1,3 @@
+export default function BookingsPage() {
+	return <h1 className="text-3xl">BookingsPage</h1>;
+}
---
+++ app/rentals/page.tsx
@@ -0,0 +1,3 @@
+export default function RentalsPage() {
+	return <h1 className="text-3xl">RentalsPage</h1>;
+}
