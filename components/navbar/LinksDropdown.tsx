4abb6fc3e80743efb7e06f1347effd21941e1fef
sivaram <sivaram3710@gmail.com>
2024-10-03 16:08:10 +0530

    added clerk auth

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -4,11 +4,14 @@ import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
+	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import UserIcon from "./UserIcon";
 import { links } from "@/utils/links";
 import Link from "next/link";
+// import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
+import SignOutLink from "./SignOutLink";
 
 export default function LinksDropdown() {
 	return (
@@ -20,11 +23,39 @@ export default function LinksDropdown() {
 				</Button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent className="w-52" align="center" sideOffset={10}>
+				{/* <SignedOut>
+					<DropdownMenuItem>
+						<SignInButton mode="modal">
+							<button className="w-full text-left">Login</button>
+						</SignInButton>
+					</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>
+						<SignUpButton mode="modal">
+							<button className="w-full text-left">Register</button>
+						</SignUpButton>
+					</DropdownMenuItem>
+				</SignedOut>
+				<SignedIn>
+					{links.map((link) => (
+						<DropdownMenuItem key={link.href}>
+							<Link href={link.href}>{link.label}</Link>
+						</DropdownMenuItem>
+					))}
+					<DropdownMenuSeparator />
+					<DropdownMenuItem>
+						<SignOutLink />
+					</DropdownMenuItem>
+				</SignedIn> */}
 				{links.map((link) => (
 					<DropdownMenuItem key={link.href}>
 						<Link href={link.href}>{link.label}</Link>
 					</DropdownMenuItem>
 				))}
+				<DropdownMenuSeparator />
+				<DropdownMenuItem>
+					<SignOutLink />
+				</DropdownMenuItem>
 			</DropdownMenuContent>
 		</DropdownMenu>
 	);
