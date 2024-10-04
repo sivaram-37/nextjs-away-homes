8e1e3b56429251c8f3afcf8d64485829ba622a8f
sivaram <sivaram3710@gmail.com>
2024-10-04 11:32:48 +0530

    show profileicon in the navbar if present

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,17 @@
+import { fetchProfileImage } from "@/utils/actions";
+import Image from "next/image";
 import { LuUser2 } from "react-icons/lu";
 
-function UserIcon() {
+async function UserIcon() {
+	const profuleImage = await fetchProfileImage();
+	if (profuleImage)
+		return (
+			<Image
+				src={profuleImage}
+				alt="profile"
+				className="w-6 h-6 rounded-full object-cover"
+			/>
+		);
 	return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 export default UserIcon;
