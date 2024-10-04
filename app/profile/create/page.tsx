e7d41eee3558e803ae16aa974cbef711ca4d79f7
sivaram <sivaram3710@gmail.com>
2024-10-04 10:41:51 +0530

    added profile/create page

---
+++ app/profile/create/page.tsx
@@ -0,0 +1,23 @@
+import FormContainer from "@/components/form/FormContainer";
+import FormInput from "@/components/form/FormInput";
+import SubmitButton from "@/components/form/SubmitButton";
+import { createProfileAction } from "@/utils/actions";
+
+function CreateProfile() {
+	return (
+		<section>
+			<h1 className="text-2xl font-semibold mb-8 capitalize">new user</h1>
+			<div className="border p-8 rounded-md ">
+				<FormContainer action={createProfileAction}>
+					<div className="grid md:grid-cols-2 gap-4 mt-4 ">
+						<FormInput type="text" name="firstName" label="First Name" />
+						<FormInput type="text" name="lastName" label="Last Name" />
+						<FormInput type="text" name="username" label="Username" />
+					</div>
+					<SubmitButton text="Create Profile" className="mt-8" />
+				</FormContainer>
+			</div>
+		</section>
+	);
+}
+export default CreateProfile;
