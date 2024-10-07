157ff3d09f979a277f33ea7b8bc0faadb860467d
sivaram <sivaram3710@gmail.com>
2024-10-07 12:01:42 +0530

    modified blob

--- utils/blob.ts
+++ utils/blob.ts
@@ -2,7 +2,7 @@ import { put } from "@vercel/blob";
 
 export default async function uploadImage(image: File) {
 	const timestamp = Date.now();
-	const newName = `${image.name}${timestamp}`;
+	const newName = `${timestamp}${image.name}`;
 
 	const blob = await put(newName, image, {
 		access: "public",
